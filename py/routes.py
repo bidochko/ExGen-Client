57c86af3ec8d4e2de502421dfd6062499858b449
steffandrosinos <39098140+steffandrosinos@users.noreply.github.com>
2019-04-01 21:33:41 +0100

    salt generation

--- py/routes.py
+++ py/routes.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, request, send_from_directory
 
 #Password Hashing
 from passlib.hash import sha256_crypt
+from saltgen import createSalt
 
 #Flask Form
 from flask_wtf import FlaskForm
@@ -17,6 +18,7 @@ from dbconnect import connect
 import gc
 import os
 
+#Secret key, tbh I don't know what this does but it complains when I dont set one
 SECRET_KEY = os.urandom(32)
 application.config['SECRET_KEY'] = SECRET_KEY
 
@@ -24,14 +26,14 @@ application.config['SECRET_KEY'] = SECRET_KEY
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/index', methods=['GET', 'POST'])
 def index():
-    error = ''
+    error = '' #temp definition
     try:
-        conn, trans = connect()
-        if request.method == "POST":
+        conn, trans = connect() #Connect SQLAlchemy Engine to MySQL
+        if request.method == "POST": #user presses "login"
             data = conn.execute("SELECT * from User where UserName = %s", thwart(request.form['username']))
             for row in data:
-                hash = row[2]
-            if sha256_crypt.using(salt="hahamemelol").verify(request.form['password'], hash):
+                hash = row[2] # 3rd collum is the hashed password
+            if sha256_crypt.verify(request.form['password'], hash): #
                 session['logged_in'] = True
                 session['username'] = request.form['username']
                 return redirect(url_for("home"))
@@ -60,13 +62,13 @@ def register():
         if request.method == "POST" and form.validate():
             name = form.name.data
             username = form.username.data
-            slt = "hahamemelol"
-            password = sha256_crypt.using(salt=slt).hash((str(form.password.data)))
+            randomSalt = createSalt()
+            password = sha256_crypt.encrypt((str(form.password.data)), salt=randomSalt, rounds=555000)
 
             conn, trans = connect()
             try:
                 type = "student"
-                conn.execute("INSERT INTO User (UserName, Hash, Salt, Type) VALUES (%s, %s, %s, %s)", (thwart(username), thwart(password), slt, type))
+                conn.execute("INSERT INTO User (UserName, Hash, Salt, Type) VALUES (%s, %s, %s, %s)", (thwart(username), thwart(password), randomSalt, type))
                 trans.commit()  # transaction is not committed yet
             except:
                 trans.rollback() # this rolls back the transaction unconditionally
@@ -108,5 +110,7 @@ def settings():
 @application.route("/hashing/")
 def hashing():
     slt = "testing"
-    hash = sha256_crypt.using(salt=slt).hash("password")
-    return hash
+    hash1 = sha256_crypt.using(salt=slt).hash("password")
+    slt2 = "woahman"
+    hash2 = sha256_crypt.using(salt=slt2).hash("password")
+    return hash2

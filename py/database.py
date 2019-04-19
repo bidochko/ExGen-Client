26d28ad22b82cb62f0291252860be9bc81a24163
Zarinie <zarinie@users.noreply.github.com>
2019-04-19 21:37:57 +0100

    Update database.py

--- py/database.py
+++ py/database.py
@@ -5,7 +5,7 @@ db_session = scoped_session(sessionmaker(autocommit=False,
                                          bind=engine))
 
 
-def create_user(username, pw_hash, pw_salt):
+def _create_user(username, pw_hash, pw_salt):
     user = User()
     user.UserName = username
     user.Hash = pw_hash
@@ -13,17 +13,21 @@ def create_user(username, pw_hash, pw_salt):
     db_session.add(user)
     db_session.commit()
     return user
+# private method
 
 
 def create_student(username, pw_hash, pw_salt):
-    user = create_user(username, pw_hash, pw_salt)
+    user = _create_user(username, pw_hash, pw_salt)
     student = Student()
     student.UserID = user.UserID
     student.isCourseRep = False
     db_session.add(student)
     db_session.commit()
 
+# Create Professor method still needs made
+
 
 def get_user_from_user_id(user_id):
     user = db_session.query(User).filter(User.UserID == user_id).first()
     return user
+

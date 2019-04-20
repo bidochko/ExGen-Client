679ddbf1684331b54e9af0bffc8b8b486fbc7ab6
steffandrosinos <39098140+steffandrosinos@users.noreply.github.com>
2019-04-21 00:57:34 +0100

    Database

---
+++ py/db/database.py
@@ -0,0 +1,157 @@
+from models import *
+
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+
+
+def _create_user(username, pw_hash, pw_salt):
+    user = User()
+    user.UserName = username
+    user.Hash = pw_hash
+    user.Salt = pw_salt
+    db_session.add(user)
+    db_session.commit()
+    return user
+# private method
+
+
+def create_student(username, pw_hash, pw_salt):
+    user = _create_user(username, pw_hash, pw_salt)
+    student = Student()
+    student.UserID = user.UserID
+    student.isCourseRep = False
+    db_session.add(student)
+    db_session.commit()
+
+
+def create_professor(username, pw_hash, pw_salt, professor_name, professor_info):
+    user = _create_user(username, pw_hash, pw_salt)
+    professor = Professor()
+    professor.UserID = user.UserID
+    professor.ProfessorName = professor_name
+    professor.ProfessorInfo = professor_info
+    db_session.add(professor)
+    db_session.commit()
+
+
+def get_user_from_user_id(user_id):
+    user = db_session.query(User).filter(User.UserID == user_id).first()
+    return user
+
+
+def get_user_from_user_name(user_name):
+    user = db_session.query(User).filter(User.UserName == user_name).first()
+    return user
+
+
+def get_student_from_user_id(user_id):
+    student = db_session.query(Student).filter(Student.UserID == user_id).first()
+    return student
+
+
+def get_professor_from_user_id(user_id):
+    professor = db_session.query(Professor).filter(Professor.UserID == user_id).first()
+    return professor
+
+
+def get_module_id_from_module_code(module_code):
+    module = db_session.query(CourseModule).filter(CourseModule.ModuleCode == module_code).first()
+    return module.ModuleID
+
+
+def add_professor_to_module(professor_id, module_id, head_professor):
+    professor_module = ProfessorModule()
+    professor_module.ProfessorID = professor_id
+    professor_module.ModuleID = module_id
+    professor_module.HeadProfessor = head_professor
+
+    db_session.add(professor_module)
+    db_session.commit()
+
+
+def create_module_given_head_professor(professor_id, module_name, module_description, module_code):
+    module = CourseModule()
+    module.ModuleName = module_name
+    module.ModuleDescription = module_description
+    module.ModuleCode = module_code
+
+    db_session.add(module)
+    db_session.commit()
+
+    module_id = get_module_id_from_module_code(module_code)
+    add_professor_to_module(professor_id, module_id, True)
+
+
+def add_student_to_module(student_id, module_id, course_representative):
+    student_module = StudentModule()
+    student_module.StudentID = student_id
+    student_module.ModuleID = module_id
+    student_module.CourseRep = course_representative
+
+    db_session.add(student_module)
+    db_session.commit()
+
+
+def get_module_id_from_student_id(student_id):
+    module = db_session.query(StudentModule).filter(StudentModule.StudentID == student_id)
+    return module
+
+# Returns a list of ModuleIDs
+
+
+def get_module_id_from_professor_id(professor_id):
+    module = db_session.query(ProfessorModule).filter(ProfessorModule.ProfessorID == professor_id)
+    return module
+
+# Returns a list of ModuleIDs
+
+
+def get_module_from_module_id(module_id):
+    module = db_session.query(CourseModule).filter(CourseModule.ModuleID == module_id)
+    return module
+
+# Returns list containing all parts of the module
+
+
+def get_all_available_modules():
+    module = db_session.query(CourseModule)
+    return module
+
+
+def delete_one_student_from_module(student_id, module_id):
+    db_session.query(StudentModule).filter(StudentModule.StudentID == student_id, StudentModule.ModuleID == module_id).delete()
+    db_session.commit
+
+
+def delete_one_professor_from_module(professor_id, module_id):
+    db_session.query(ProfessorModule).filter(ProfessorModule.ProfessorID == professor_id, ProfessorModule.ModuleID == module_id).delete()
+    db_session.commit
+
+
+# Think about any difference for head professors
+
+def delete_all_students_from_module(module_id):
+    db_session.query(StudentModule).filter(StudentModule.ModuleID == module_id).delete()
+    db_session.commit
+
+
+def delete_all_professors_from_module(module_id):
+    db_session.query(ProfessorModule).filter(ProfessorModule.ModuleID == module_id).delete()
+    db_session.commit
+
+
+def delete_module(module_id):
+    delete_all_students_from_module(module_id)
+    delete_all_professors_from_module(module_id)
+    db_session.query(CourseModule).filter(CourseModule.ModuleID == module_id).delete()
+    db_session.commit
+
+
+def get_full_module_list_from_student_id(student_id):
+    module_id_list = get_module_id_from_student_id(student_id)
+    module_list = []
+    for module_id in module_id_list:
+        module = db_session.query(CourseModule).filter(CourseModule.ModuleID == module_id).first()
+        module_list.extend(module)
+    return module_list
---
+++ py/db/models.py
@@ -0,0 +1,132 @@
+import datetime
+
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Table, BigInteger, Time, Boolean, text
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from config import db_user, db_password, db_host, db_name, db_port
+
+Base = declarative_base()
+metadata = Base.metadata
+engine = create_engine('mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(db_user, db_password, db_host, db_port, db_name))
+
+
+class User(Base):
+    __tablename__ = "User"
+    UserID = Column(Integer, autoincrement=True, primary_key=True, unique=True, nullable=False)
+    UserName = Column(String(128), nullable=False, unique=True)
+    Hash = Column(String(128), nullable=False)
+    Salt = Column(String(128), nullable=False)
+    Professor = relationship("Professor", uselist=False)
+    Student = relationship("Student", uselist=False)
+    Answered = relationship("Answered")
+
+
+class Professor(Base):
+    __tablename__ = "Professor"
+    ProfessorID = Column(Integer, autoincrement=True, primary_key=True, unique=True, nullable=False)
+    ProfessorName = Column(String(128), nullable=False)
+    ProfessorInfo = Column(String(128))
+
+    UserID = Column(ForeignKey('User.UserID'), primary_key=False, nullable=False, index=True)
+    User = relationship('User')
+    ProfessorModule = relationship('ProfessorModule')
+
+
+
+class Student(Base):
+    __tablename__ = "Student"
+    StudentID = Column(Integer, autoincrement=True, primary_key=True, unique=True, nullable=False)
+    isCourseRep = Column(Boolean, nullable=False)
+
+    UserID = Column(ForeignKey('User.UserID'), primary_key=False, nullable=False, index=True)
+    User = relationship('User')
+    StudentModule = relationship('StudentModule')
+
+
+class CourseModule(Base):
+    __tablename__ = "CourseModule"
+    ModuleID = Column(Integer, autoincrement=True, primary_key=True, unique=True, nullable=False)
+    ModuleName = Column(String(128), nullable=False, unique=True)
+    ModuleDescription = Column(String(128))
+    ModuleCode = Column(String(128), nullable=False)
+    Exam = relationship('Exam')
+    StudentModule = relationship('StudentModule')
+    ProfessorModule = relationship('ProfessorModule')
+
+
+class Exam(Base):
+    __tablename__ = "Exam"
+    ExamID = Column(Integer, autoincrement=True, primary_key=True, unique=True, nullable=False)
+    Title = Column(String(32), nullable=False)
+    Description = Column(String(128))
+    Enabled = Column(Boolean, nullable=False)
+
+    ModuleID = Column(ForeignKey('CourseModule.ModuleID'), primary_key=False, nullable=False, index=True)
+    CourseModule = relationship('CourseModule')
+
+
+class Question(Base):
+    __tablename__ = "Question"
+    QuestionTemplateID = Column(Integer, autoincrement=True, primary_key=True, unique=True, nullable=False)
+    LaTeX = Column(String(128), nullable=False)
+    SolutionCode = Column(String(128), nullable=False)
+    Enabled = Column(Boolean, nullable=False)
+
+
+class Variable(Base):
+    __tablename__ = "Variable"
+    VariableID = Column(Integer, autoincrement=True, primary_key=True, unique=True, nullable=False)
+    VariableName = Column(String(32), nullable=False)
+    VariableValue = Column(Integer, nullable=False)
+
+
+class Answered(Base):
+    __tablename__ = "Answered"
+    QuestionID = Column(Integer, autoincrement=True, primary_key=True, unique=True, nullable=False)
+    Correct = Column(Boolean, nullable=False)
+
+    UserID = Column(ForeignKey('User.UserID'), primary_key=False, nullable=False, index=True)
+    User = relationship("User")
+
+
+class StudentModule(Base):
+    __tablename__ = "StudentModule"
+    StudentID = Column(Integer, primary_key=True,  nullable=False)
+    ModuleID = Column(Integer, primary_key=True, nullable=False)
+    CourseRep = Column(Boolean, nullable=False)
+
+    StudentID = Column(ForeignKey('Student.StudentID'), primary_key=True, nullable=False, index=True)
+    Student = relationship("Student")
+    ModuleID = Column(ForeignKey('CourseModule.ModuleID'), primary_key=True, nullable=False, index=True)
+    CourseModule = relationship("CourseModule")
+
+
+class ProfessorModule(Base):
+    __tablename__ = "ProfessorModule"
+    ProfessorID = Column(Integer, primary_key=True, nullable=False)
+    ModuleID = Column(Integer, primary_key=True, nullable=False)
+    HeadProfessor = Column(Boolean, nullable=False)
+
+    ProfessorID = Column(ForeignKey('Professor.ProfessorID'), primary_key=True, nullable=False, index=True)
+    Professor = relationship("Professor")
+    ModuleID = Column(ForeignKey('CourseModule.ModuleID'), primary_key=True, nullable=False, index=True)
+    CourseModule = relationship("CourseModule")
+
+
+t_Exam_Question = Table(
+    'Exam_Question', metadata,
+    Column('ExamID', ForeignKey('Exam.ExamID'), primary_key=True, nullable=False, index=True),
+    Column('QuestionTemplateID', ForeignKey('Question.QuestionTemplateID'), primary_key=True, nullable=False,
+           index=True)
+)
+
+t_Variable_Question = Table(
+    'Variable_Question', metadata,
+    Column('VariableID', ForeignKey('Variable.VariableID'), primary_key=True, nullable=False, index=True),
+    Column('QuestionTemplateID', ForeignKey('Question.QuestionTemplateID'), primary_key=True, nullable=False,
+           index=True),
+    Column('QuestionID', ForeignKey('Answered.QuestionID'), primary_key=True, nullable=False, index=True)
+)
+
+metadata.create_all(bind=engine)
